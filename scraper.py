2e36e7afe9b6b2ec4fe2d6adf328d6ea73220053
Ryan McCormick <rmccorm4@binghamton.edu>
2018-05-01 13:45:35 -0400

    finish modifying scraper to work with different parameters, and return filename for wallpaper script

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ import random
 # adapted from http://stackoverflow.com/questions/20716842/python-download-images-from-google-image-search
 
 def get_soup(url,header):
-    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
+	return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
 '''
 def scrape(args):
@@ -24,15 +24,19 @@ def scrape(args):
 	max_images = args.num_images
 	save_directory = args.directory
 '''
+
 def scrape(query, save_directory):
 
 	image_type="Action"
 	
+	search_term = query[:]
 	# Clean up query
 	if query != 'desktop background 1080p':
 		# If a search term specific like 'Space' or 'Cats', append
 		# desktop background description to the query for wallpapers
 		query += ' desktop background 1080p' 
+	else:
+		search_term = 'random'
 
 	query= query.split()
 	query='+'.join(query)
@@ -41,32 +45,38 @@ def scrape(query, save_directory):
 	soup = get_soup(url,header)
 	ActualImages=[]# contains the link for Large original images, type of  image
 	for a in soup.find_all("div",{"class":"rg_meta"}):
-	    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-	    ActualImages.append((link,Type))
+		link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+		ActualImages.append((link,Type))
 	
 	# Get random image from search results so its different every time
-	rand_image_num = random.randint(0, 99)
+	# Only looking at first 30 for best results with some diversity still
+	rand_image_num = random.randint(0, 30)
 	#for i , (img , Type) in enumerate( ActualImages[0:max_images]):
 	# Kept as this syntax so I didn't have to change any code, ignore the loop
 	# despite it being for a single image
 	for i , (img , Type) in enumerate( ActualImages[rand_image_num:rand_image_num+1]):
-	    try:
-	        req = urllib.request.Request(img, headers=header)
-	        raw_img = urllib.request.urlopen(req).read()
-	        if len(Type)==0:
-	            f = open(os.path.join(save_directory , "img" + "_"+ str(i)+".jpg"), 'wb')
-	        else :
-	            f = open(os.path.join(save_directory , "img" + "_"+ str(i)+"."+Type), 'wb')
-	        f.write(raw_img)
-	        f.close()
-	    except Exception as e:
-	        print("could not load : " + img)
-	        print(e)
+		try:
+			req = urllib.request.Request(img, headers=header)
+			raw_img = urllib.request.urlopen(req).read()
+			if len(Type)==0:
+				retval = "img_" + search_term +".jpg"
+				f = open(os.path.join(save_directory, retval), 'wb')
+			else :
+				retval = "img_" + search_term +"."+Type
+				f = open(os.path.join(save_directory, retval), 'wb')
+			f.write(raw_img)
+			f.close()
+		except Exception as e:
+			print("could not load : " + img)
+			print(e)
+	
+	# Return filename created
+	return retval
 
 if __name__ == '__main__':
-    from sys import argv
-    try:
-        scrape(argv)
-    except KeyboardInterrupt:
-        pass
-    sys.exit()
+	from sys import argv
+	try:
+		scrape(argv)
+	except KeyboardInterrupt:
+		pass
+	sys.exit()
